14d538728b12ab4240ca0c79ed209d58d1242730
John Spencer IV <32145224+JSpencerIV@users.noreply.github.com>
2018-01-24 16:17:11 -0800

    Initial commit

---
+++ WebFormPractice1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormPractice1.aspx.cs" Inherits="MyFirstChallenge.WebFormPractice1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How old are you?&nbsp;
+            <asp:TextBox ID="yourAgeTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Where are you from?&nbsp;
+            <asp:TextBox ID="yourLocationTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="Button1_Click" Text="Click Here!" Height="26px" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormPractice1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class WebFormPractice1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          string yourAge = yourAgeTextBox.Text;                 /* string = size,
+                                                                 * yourAge = variable name,
+                                                                 * "=" is assignment, 
+                                                                 * ".Text" is placement */
+//In line 19 I declared variable (yourAge) then immediately *initialize (to give value to newly created variable.), (= yourAgeTextBox.Text)
+            string yourLocation = yourLocationTextBox.Text;
+
+          string result = "You are " + yourAge + " years old. " + " " + "And live in " + yourLocation;
+        
+          resultLabel.Text = result;
+        }
+    }
+}
